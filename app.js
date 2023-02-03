02b8ed1bb40d6d2916c637752ffebc6bdf590bf3
Luke <luke.michalek@gmail.com>
2023-02-03 20:56:16 +0000

    OAuth implemented

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 /* 
  * Package Imports
 */
+const session = require('express-session');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const path = require("path");
 require("dotenv").config();
 const express = require('express');
 const partials = require('express-partials');
+
+
 
 
 const app = express();
@@ -23,6 +28,24 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
  * Passport Configurations
 */
 
+passport.use(new GitHubStrategy({
+
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/github/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  return done (null, profile);
+}
+  
+));
+
+passport.serializeUser((user,done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user,done) => {
+  done(null,user);
+});
 
 
 
@@ -33,13 +56,38 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
  *  Express Project Setup
 */
 
+// 
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+app.use(
+  session({
+    secret: 'codecademy',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
+app.use(passport.initialize());
+
+
+
+app.use(passport.session());
+
+
+const ensureAuthenticated = (req, res, next) => {
+  if(req.isAuthenticated()){
+    return next()
+  }
+  else{
+    res.redirect('/login')
+  }
+}
 
 
 /*
@@ -50,7 +98,7 @@ app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated,(req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -62,6 +110,10 @@ app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
+
+app.get('/auth/github', passport.authenticate('github', {scope: ['user']}))
+
+app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/login', successRedirect: '/'}))
 
 
 
